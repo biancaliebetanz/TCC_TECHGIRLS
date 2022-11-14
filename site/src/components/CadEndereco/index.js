342664f1150b644cd0cfadc59e95c129188b7a3f
biancaliebetanz <biancalbtz@gmail.com>
2022-11-14 16:02:58 -0300

    Página do usuário ok

--- site/src/components/CadEndereco/index.js
+++ site/src/components/CadEndereco/index.js
@@ -23,9 +23,9 @@ export default function CadastroEndereco() {
         catch (err) {
             toast.error(err.response.data.erro);
         }
-    }   
+    }
 
-    
+
 
 
     return (
@@ -33,60 +33,60 @@ export default function CadastroEndereco() {
             <div>
                 <div className='conteudo'>
                     <div className='endereco'>
-                    <img  src='./../../../images/casaEndereco.png'></img>
-                    <p> Endereço </p>
+                        <img src='./../../../images/casaEndereco.png'></img>
+                        <p> Endereço </p>
                     </div>
 
                     <div className='form'>
-                        
+
+                    </div>
+                    <div className='endereco1'>
+                        <div className='div1'>
+                            <p> CEP</p>
+                            <input type='text' value={cep} onChange={e => setCEP(e.target.value)} />
                         </div>
-                        <div className='endereco1'>
-                            <div className='div1'>
-                                <p> CEP</p>
-                                <input type='text' value={cep}  onChange={e => setCEP(e.target.value)}  />
-                            </div>
-                            <div className='div2'>
-                                <p> Referência</p>
-                                <input type='text' value={referencia} onChange={e => setReferencia(e.target.value)} />
-                            </div>
+                        <div className='div2'>
+                            <p> Referência</p>
+                            <input type='text' value={referencia} onChange={e => setReferencia(e.target.value)} />
                         </div>
-                        <div>
-                           
+                    </div>
+                    <div>
+
                         <label> &nbsp; </label>
 
 
                         <div className='endereco1'>
                             <div className='div3'>
-                                    <p>Endereço</p>
-                                    <input type='text' value={endereco}  onChange={e => setEndereco(e.target.value)}  />
-                                </div>
+                                <p>Endereço</p>
+                                <input type='text' value={endereco} onChange={e => setEndereco(e.target.value)} />
+                            </div>
                             <div className='div4'>
                                 <p> Número</p>
-                                <input type='text' value={numero}  onChange={e => setNumero(e.target.value)}  />
+                                <input type='text' value={numero} onChange={e => setNumero(e.target.value)} />
+                            </div>
                         </div>
-                       </div>
-                        
+
                         <div>
                             <label> &nbsp; </label>
                         </div>
-                        
+
                         <div className='endereco1'>
                             <div>
                                 <p> Bairro</p>
-                                <input type='text' value={bairro}  onChange={e => setBairro(e.target.value)}  />
+                                <input type='text' value={bairro} onChange={e => setBairro(e.target.value)} />
                             </div>
                             <div className='div6'>
                                 <p> Estado</p>
-                                <input type='text' value={estado}  onChange={e => setEstado(e.target.value)}  />
+                                <input type='text' value={estado} onChange={e => setEstado(e.target.value)} />
                             </div>
                             <div className='div6'>
                                 <p> Cidade</p>
-                                <input type='text' value={cidade}  onChange={e => setCidade(e.target.value)}  />
+                                <input type='text' value={cidade} onChange={e => setCidade(e.target.value)} />
                             </div>
                         </div>
-                        
+
                         <div>
-                        
+
                             <div className='btn'>
                                 <button onClick={salvarEndereco}> Salvar </button>
                             </div>
