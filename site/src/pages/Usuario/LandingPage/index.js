256e0b8e7388b0e221680b24f9b7348f6551122a
biancaliebetanz <biancalbtz@gmail.com>
2022-11-18 21:58:01 -0300

    alteração na constante breakpoints do carrossel de banner

--- site/src/pages/Usuario/LandingPage/index.js
+++ site/src/pages/Usuario/LandingPage/index.js
@@ -20,9 +20,9 @@ export default function Index() {
 
     const breakPoints = [
         { width: 1, itemsToShow: 1 },
-        { width: 1200, itemsToShow: 2 },
-        { width: 1200, itemsToShow: 3 },
-        { width: 1200, itemsToShow: 4 },
+        { width: 1200, itemsToShow: 1 },
+        { width: 1200, itemsToShow: 1 },
+        { width: 1200, itemsToShow: 1 },
     ];
 
     const [produtos, setProdutos] = useState([]);
